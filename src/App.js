a2a396856b21ba9b55651fe236ed42984b5a6a33
Valentina Troiano <valetroiano.vt@gmail.com>
2021-11-01 15:25:49 -0300

    agregando nav bar desafio 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import reactDom from 'react-dom';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+     
+     <header className="navbar">
+     
+     <button type="button" className="btn btn-dark">Home</button>
+     <button type="button" className="btn btn-dark">Informacion</button>
+     <button type="button" className="btn btn-dark">Contacto</button>
+     <form class="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+     </header>
+   </React.Fragment>
   );
 }
 
